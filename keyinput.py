90d7d620b2eed194935a37b65705edde42800c2d
Alex_Dyke <dykeag@gmail.com>
2014-12-01 18:59:08 +0000

    Runtime debug

--- keyinput.py
+++ keyinput.py
@@ -8,10 +8,15 @@ import atexit
 from motor import Motor
 
 
-fan1   = Motor(0, 1300, 2600, 1300,  4, "Fan 1");
-fan2   = Motor(1, 1300, 2600, 1300,  5, "Fan 2");
-servo1 = Motor(2, 1025, 2750, 1025, -1, "Servo 1");
-servo2 = Motor(3, 1025, 2750, 1025, -1, "Servo 2");
+fan1   = Motor(0, 1300, 2600, 0,  4, "Fan 1");
+fan2   = Motor(1, 1300, 2600, 0,  5, "Fan 2");
+servo1 = Motor(2, 1025, 2750, 2, -1, "Servo 1");
+servo2 = Motor(3, 1025, 2750, 90, -1, "Servo 2");
+
+#servo3 = Motor(6, 1025, 2750, 1025, -1, "pan");
+#servo4 = Motor(7, 1025, 2750, 1025, -1, "tilt");
+servo3 = Motor(6, 0, 4095, 0, -1, "pan");
+servo4 = Motor(7, 0, 4095, 0, -1, "tilt");
 
 
 
@@ -27,108 +32,139 @@ stdscr.insstr("q: exit | w: max | s: min | h: half | p: off | 0-9: select output
 #stdscr.insertln()
 
 curMotor = fan1;
+curMotor = servo3;
 stdscr.move(3,0)
 stdscr.deleteln()
 stdscr.insertln()
 stdscr.insstr('Using Motor: 1 (%s)'% curMotor.getName())
 
 
+stdscr.move(5,0)
+stdscr.deleteln()
+stdscr.insertln()
+stdscr.insstr('Freq: ' + str(Motor.getFreq()))
 
 while True:
-	c = stdscr.getch()
-	
-	#go to first line, replace it with the key pressed
-	stdscr.move(0,0)
-	stdscr.deleteln()
-	stdscr.insertln()
-	#stdscr.insstr('you pressed %s (code %s)' % (chr(c), c))
+  c = stdscr.getch()
+  
+  #go to first line, replace it with the key pressed
+  stdscr.move(0,0)
+  stdscr.deleteln()
+  stdscr.insertln()
+  #stdscr.insstr('you pressed %s (code %s)' % (chr(c), c))
 
 
-	if (chr(c) == 'q'):
-		Motor.allOff();
-		curses.endwin()
-		break;
-	elif (chr(c) == 'w'):
-		#go to second line. Replace it with current command
-		stdscr.move(1,0)
-		stdscr.deleteln()
-		stdscr.insertln()
-		stdscr.insstr('SETING SERVO MAX')
-		curMotor.goMax();
+  if (chr(c) == 'q'):
+    Motor.allOff();
+    curses.endwin()
+    break;
+  '''
+  elif (chr(c) == 'j'):
+    #lower frequency
+    f = Motor.getFreq()
+    f = f - 10
+    if f < 0: f = 0
+    stdscr.move(5,0)
+    stdscr.deleteln()
+    stdscr.insertln()
+    stdscr.insstr('Freq: ' + str(f))
 
-	elif (chr(c) == 's'):
-		#go to second line. Replace it with current command
-		stdscr.move(1,0)
-		stdscr.deleteln()
-		stdscr.insertln()
-		stdscr.insstr('SETING SERVO MIN')
-		curMotor.goMin();
-	elif (chr(c) == 'p'):
-		#go to second line. Replace it with current command
-		stdscr.move(1,0)
-		stdscr.deleteln()
-		stdscr.insertln()
-		stdscr.insstr('ALL SERVOS OFF')
-		Motor.allOff();
-	elif (chr(c) == 'h'):
-		#go to second line. Replace it with current command
-		stdscr.move(1,0)
-		stdscr.deleteln()
-		stdscr.insertln()
-		stdscr.insstr('SETING SERVO HALF')
-		curMotor.goHalf();
-	elif (c == 65):
-		#go to second line. Replace it with current command
-		stdscr.move(1,0)
-		stdscr.deleteln()
-		stdscr.insertln()
+    Motor.setFreq(f)
 
-		if (curMotor.incr() != 0):
-			stdscr.insstr('Already at maximum duty cycle!')
-		else:
-			stdscr.insstr('Increasing to: %d (%d %%)' % (curMotor.getValue(), curMotor.getPercent()))
-			
-	elif (c == 66):
-		#go to second line. Replace it with current command
-		stdscr.move(1,0)
-		stdscr.deleteln()
-		stdscr.insertln()
+  elif (chr(c) == 'k'):
+    #incr frequency
+    f = Motor.getFreq()
+    f = f + 10
+    if f > 300: f = 300
+    stdscr.move(5,0)
+    stdscr.deleteln()
+    stdscr.insertln()
+    stdscr.insstr('Freq: ' + str(f))
 
-		if (curMotor.decr() != 0):
-			stdscr.insstr('Already at minimum duty cycle!')
-		else:
-			stdscr.insstr('Decreasing to: %d (%d %%)' % (curMotor.getValue(), curMotor.getPercent()))
-	
-	elif (c >= 49 and c <= 57): #number 1
+    Motor.setFreq(f)
+  '''
+  if (chr(c) == 'w'):
+    #go to second line. Replace it with current command
+    stdscr.move(1,0)
+    stdscr.deleteln()
+    stdscr.insertln()
+    stdscr.insstr('SETING SERVO MAX')
+    curMotor.goMax();
 
-		invalid = False;
-		stdscr.move(1,0)
-		stdscr.deleteln()
-		stdscr.insertln()
+  elif (chr(c) == 's'):
+    #go to second line. Replace it with current command
+    stdscr.move(1,0)
+    stdscr.deleteln()
+    stdscr.insertln()
+    stdscr.insstr('SETING SERVO MIN')
+    curMotor.goMin();
+  elif (chr(c) == 'p'):
+    #go to second line. Replace it with current command
+    stdscr.move(1,0)
+    stdscr.deleteln()
+    stdscr.insertln()
+    stdscr.insstr('ALL SERVOS OFF')
+    Motor.allOff();
+  elif (chr(c) == 'h'):
+    #go to second line. Replace it with current command
+    stdscr.move(1,0)
+    stdscr.deleteln()
+    stdscr.insertln()
+    stdscr.insstr('SETING SERVO HALF')
+    curMotor.goHalf();
+  elif (c == 65):
+    #go to second line. Replace it with current command
+    stdscr.move(1,0)
+    stdscr.deleteln()
+    stdscr.insertln()
 
-		if (c == 49):
-			curMotor = fan1;
-		elif (c == 50):
-			curMotor = fan2;
-		elif (c == 51):
-			curMotor = servo1;
-		elif (c == 52):
-			curMotor = servo2;
-		else:
-			invalid=True
-		if(invalid):
-			curMotor=fan1;
-			stdscr.insstr('INVALID SELECTION')
-		else:
-			stdscr.insstr('SELECTING MOTOR %s %s' % (chr(c), curMotor.getName()))
-			stdscr.move(3,0)
-			stdscr.deleteln()
-			stdscr.insertln()
-			stdscr.insstr('Using Motor: %s (%s)'% (chr(c), curMotor.getName()))
+    if (curMotor.incr() != 0):
+      stdscr.insstr('Already at maximum duty cycle!')
+    else:
+      stdscr.insstr('Increasing to: %d (%d %%)' % (curMotor.getValue(), curMotor.getPercent()))
+      
+  elif (c == 66):
+    #go to second line. Replace it with current command
+    stdscr.move(1,0)
+    stdscr.deleteln()
+    stdscr.insertln()
 
+    if (curMotor.decr() != 0):
+      stdscr.insstr('Already at minimum duty cycle!')
+    else:
+      stdscr.insstr('Decreasing to: %d (%d %%)' % (curMotor.getValue(), curMotor.getPercent()))
+  
+  elif (c >= 49 and c <= 57): #number 1
 
+    invalid = False;
+    stdscr.move(1,0)
+    stdscr.deleteln()
+    stdscr.insertln()
 
-	
+    if (c == 49):
+      curMotor = fan1;
+    elif (c == 50):
+      curMotor = fan2;
+    elif (c == 51):
+      curMotor = servo1;
+    elif (c == 52):
+      curMotor = servo2;
+    elif (c == 53):
+      curMotor = servo3;
+    elif (c == 54):
+      curMotor = servo4;
+    else:
+      invalid=True
+
+    if(invalid):
+      curMotor=fan1;
+      stdscr.insstr('INVALID SELECTION')
+    else:
+      stdscr.insstr('SELECTING MOTOR %s %s' % (chr(c), curMotor.getName()))
+      stdscr.move(3,0)
+      stdscr.deleteln()
+      stdscr.insertln()
+      stdscr.insstr('Using Motor: %s (%s)'% (chr(c), curMotor.getName()))
 
 
 
--- motor.py
+++ motor.py
@@ -11,171 +11,178 @@ from Adafruit_PWM_Servo_Driver import PWM
 
 
 class Motor:
-	
-	pwm = PWM(0x40, debug=False)
-	init = False;
+  
+  pwm = PWM(0x40, debug=False)
+  init = False;
 
-	freq=300 #PWM frequency
-	FORWARD = 1
-	REVERSE = 2
-	STOPTIME = .5 #time it takes for motor to come to complete stop
+  freq=300 #PWM frequency
+  FORWARD = 1
+  REVERSE = 2
+  STOPTIME = .5 #time it takes for motor to come to complete stop
 
-	@staticmethod
-	def allOff():
-		Motor.pwm.setAllPWM(0,0)
-	
-	@staticmethod
-	def initPWM():
-		if (Motor.init == False):
-			Motor.pwm.setPWMFreq(Motor.freq)
-			Motor.init = True
+  @staticmethod
+  def allOff():
+    Motor.pwm.setAllPWM(0,0)
+  
+  @staticmethod
+  def initPWM():
+    if (Motor.init == False):
+      Motor.pwm.setPWMFreq(Motor.freq)
+      Motor.init = True
 
-	def __init__(self, pwmport, setMin, setMax, startVal=0, revPort=-1, name="", inv=False):
-		if (netconstants.DEBUG): print "Init Motor: ", name, " on ", pwmport
-		Motor.initPWM();
+  @staticmethod
+  def setFreq(freq):
+    Motor.freq = freq
+    return Motor.pwm.setPWMFreq(freq);
 
-		self.port = pwmport;
-		self.revPort = revPort;
-		self.invert = inv; #invert the percentage (100 <-> 0)
+  @staticmethod
+  def getFreq():
+    return Motor.freq
 
-		self.name = name;
+  def __init__(self, pwmport, setMin, setMax, startVal=0, revPort=-1, name="", inv=False):
+    if (netconstants.DEBUG): print "Init Motor: ", name, " on ", pwmport
+    Motor.initPWM();
 
-		self.startValue = startVal;
-		self.pwmmin = setMin;
-		self.pwmmax = setMax;
+    self.port = pwmport;
+    self.revPort = revPort;
+    self.invert = inv; #invert the percentage (100 <-> 0)
 
-		#for documentation purposes, note these vars are available
-		self.direction = Motor.FORWARD;
-		self.value = 0;
-		self.resting = False;   #true if motors are 'reversed' but off
-	
-		self.reset();
+    self.name = name;
 
-	#reset to default state
-	def reset(self):
-		self.setDir(Motor.FORWARD);
-		self.setPercent(self.startValue);
+    self.startValue = startVal;
+    self.pwmmin = setMin;
+    self.pwmmax = setMax;
 
-	def getName(self):
-		return self.name;
-	def setName(self, newname):
-		self.name=newname;
+    #for documentation purposes, note these vars are available
+    self.direction = Motor.FORWARD;
+    self.value = 0;
+    self.resting = False;   #true if motors are 'reversed' but off
+  
+    self.reset();
 
-	def getValue(self):
-		return self.value;
+  #reset to default state
+  def reset(self):
+    self.setDir(Motor.FORWARD);
+    self.setPercent(self.startValue);
 
-	def getPercent(self):
-		
-		p = (float(self.value - self.pwmmin) / 
-			float(self.pwmmax - self.pwmmin)) * 100;
-		if self.invert: return 100-p
-		else: return p
+  def getName(self):
+    return self.name;
+  def setName(self, newname):
+    self.name=newname;
 
-	#percent is an integer [0-100]
-	def setPercent(self, percent):
+  def getValue(self):
+    return self.value;
 
-		if self.invert: percent = 100 - percent
+  def getPercent(self):
+    
+    p = (float(self.value - self.pwmmin) / 
+      float(self.pwmmax - self.pwmmin)) * 100;
+    if self.invert: return 100-p
+    else: return p
 
-		if percent < 0 or percent > 100:
-			return -1
+  #percent is an integer [0-100]
+  def setPercent(self, percent):
 
-		return self._setValue(self.pwmmin + 
-			int(float(self.pwmmax - self.pwmmin) *
-			 float(float(percent)/float(100))))
+    if self.invert: percent = 100 - percent
 
-	def _setValue(self, newvalue):
+    if percent < 0 or percent > 100:
+      return -1
 
-		ret = 0 ;
-		if(newvalue < self.pwmmin):
-			self.value = self.pwmmin;
-			ret = -1;
-		elif(newvalue > self.pwmmax):
-			self.value = self.pwmmax;
-			ret = 1;
-		else:
-			self.value=newvalue;
+    return self._setValue(self.pwmmin + 
+      int(float(self.pwmmax - self.pwmmin) *
+       float(float(percent)/float(100))))
 
-		Motor.pwm.setPWM(self.port, 0, self.value)
+  def _setValue(self, newvalue):
 
-	        return ret;
+    ret = 0 ;
+    if(newvalue < self.pwmmin):
+      self.value = self.pwmmin;
+      ret = -1;
+    elif(newvalue > self.pwmmax):
+      self.value = self.pwmmax;
+      ret = 1;
+    else:
+      self.value=newvalue;
 
-	def getMax(self):
-		return self.pwmmax;
-	def setMax(self, newmax):
-		self.pwmmax = newmax;
-	def goMax(self):
-		return self.setPercent(100);
-	def getMin(self):
-		return self.pwmmin;
-	def setMin(self, newmin):
-		self.pwmmin = newmin;
-	def goMin(self):
-		return self.setPercent(0);
-	def goHalf(self):
-		return self.setPercent(50);
+    Motor.pwm.setPWM(self.port, 0, self.value)
 
-	def incr(self, incr=10):
-		return self._setValue(self.value + incr);
+    return ret;
 
-	def decr(self, decr=10):
-		return self._setValue(self.value - decr);
+  def getMax(self):
+    return self.pwmmax;
+  def setMax(self, newmax):
+    self.pwmmax = newmax;
+  def goMax(self):
+    return self.setPercent(100);
+  def getMin(self):
+    return self.pwmmin;
+  def setMin(self, newmin):
+    self.pwmmin = newmin;
+  def goMin(self):
+    return self.setPercent(0);
+  def goHalf(self):
+    return self.setPercent(50);
 
-	def setFreq(self, freq):
-		return Motor.pwm.setPWMFreq(freq);
-	
+  def incr(self, incr=10):
+    return self._setValue(self.value + incr);
 
+  def decr(self, decr=10):
+    return self._setValue(self.value - decr);
 
-	#TODO: IMPLEMENT HARDWARE DIRECTION CHANGE
-	#0 - no change
-	#1 - forward
-	#2 - reverse
-	#as a safety feature, the motors are stopped before changing direction
-	def setDir(self, d):
-
-		if (netconstants.DEBUG):
-			print "setDir: ", d
-			print "revPort: ", self.revPort
-
-		if(self.revPort < 0 or self.revPort > 15):
-			return -1; #we cannot reverse this motor
-
-		prevval = self.getValue()
+  
 
 
-		if (d==Motor.FORWARD):
-			if (netconstants.DEBUG): print "\tFORWARD"
-			if(self.direction == Motor.REVERSE and prevval != self.getMin()):
-				self.goMin();
-				sleep(Motor.STOPTIME);
-				self._forwardRelay()
-				self.direction = Motor.FORWARD
-				self._setValue(prevval);
-			else:
-				self._forwardRelay()
-				self.direction = Motor.FORWARD
-		elif (d==Motor.REVERSE):
-			if (netconstants.DEBUG): print "\tREVERSE"
-			if(self.direction == Motor.FORWARD and prevval != self.getMin()):
-				self.goMin();
-				sleep(Motor.STOPTIME);
-				self._revRelay()
-				self.direction = Motor.REVERSE
-				self._setValue(prevval);
-			else:
-				self._revRelay()
-				self.direction = Motor.REVERSE
-		elif (d==0):
-			if (netconstants.DEBUG): print "\tNO CHANGE"
-		else:
-			if (netconstants.DEBUG): print "\tERROR"
+  #TODO: IMPLEMENT HARDWARE DIRECTION CHANGE
+  #0 - no change
+  #1 - forward
+  #2 - reverse
+  #as a safety feature, the motors are stopped before changing direction
+  def setDir(self, d):
 
-	def _revRelay(self):
-			Motor.pwm.setPWM(self.revPort, 4096, 0)
-	def _forwardRelay(self):
-			Motor.pwm.setPWM(self.revPort,0,0)
+    if (netconstants.DEBUG):
+      print "setDir: ", d
+      print "revPort: ", self.revPort
 
-	def getDir(self):
-		return self.direction
+    if(self.revPort < 0 or self.revPort > 15):
+      return -1; #we cannot reverse this motor
+
+    prevval = self.getValue()
+
+
+    if (d==Motor.FORWARD):
+      if (netconstants.DEBUG): print "\tFORWARD"
+      if(self.direction == Motor.REVERSE and prevval != self.getMin()):
+        self.goMin();
+        sleep(Motor.STOPTIME);
+        self._forwardRelay()
+        self.direction = Motor.FORWARD
+        self._setValue(prevval);
+      else:
+        self._forwardRelay()
+        self.direction = Motor.FORWARD
+    elif (d==Motor.REVERSE):
+      if (netconstants.DEBUG): print "\tREVERSE"
+      if(self.direction == Motor.FORWARD and prevval != self.getMin()):
+        self.goMin();
+        sleep(Motor.STOPTIME);
+        self._revRelay()
+        self.direction = Motor.REVERSE
+        self._setValue(prevval);
+      else:
+        self._revRelay()
+        self.direction = Motor.REVERSE
+    elif (d==0):
+      if (netconstants.DEBUG): print "\tNO CHANGE"
+    else:
+      if (netconstants.DEBUG): print "\tERROR"
+
+  def _revRelay(self):
+      Motor.pwm.setPWM(self.revPort, 4096, 0)
+  def _forwardRelay(self):
+      Motor.pwm.setPWM(self.revPort,0,0)
+
+  def getDir(self):
+    return self.direction
 
 
 
